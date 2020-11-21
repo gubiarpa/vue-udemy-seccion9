6bf1f0a44384ea6c4a44648f635526957ce9bfd4
gubiarpa <billy.arredondo@pucp.pe>
2020-11-21 05:49:27 -0500

    Ordering in a new component Input

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,72 +3,7 @@
   <h1 class="my-5">Formularios con Vue.js</h1>
 
   <form @submit.prevent="procesarFormulario">
-    <input 
-      type="text"
-      class="form-control my-2"
-      placeholder="Ingrese nombre"
-      v-model.trim="tarea.nombre"
-    >
-
-    <div class="form-check form-check-inline">
-      <input 
-        type="checkbox"
-        id="check-1"
-        class="form-check-input"
-        v-model="tarea.categorias"
-        value="javascript"
-      >
-      <label for="check-1" class="form-check-label">Javascript</label>
-    </div>
-    <div class="form-check form-check-inline">
-      <input 
-        type="checkbox"
-        id="check-2"
-        class="form-check-input"
-        v-model="tarea.categorias"
-        value="node js"
-      >
-      <label for="check-2" class="form-check-label">Node.js</label>
-    </div>
-
-    <div class="mt-2">
-      <div class="form-check form-check-inline">
-        <input 
-          type="radio"
-          id="radio-1"
-          class="form-check-input"
-          value="urgente"
-          v-model="tarea.estado"
-        >
-        <label for="radio-1" class="form-check-label">Urgente</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input 
-          type="radio"
-          id="radio-2"
-          class="form-check-input"
-          value="relax"
-          v-model="tarea.estado"
-        >
-        <label for="radio-2" class="form-check-label">Relax</label>
-      </div>
-    </div>
-
-    <div class="mt-2">
-      <input 
-        type="number"
-        class="form-control"
-        v-model.number="tarea.numero"
-      >
-    </div>
-
-    <button 
-      class="btn btn-dark mt-2 btn-block" 
-      type="submit"
-      :disabled="bloquear"
-    >
-      Procesar
-    </button>
+    <Input :tarea="tarea" />
   </form>
   <hr>
   <p>
@@ -77,16 +12,17 @@
 </template>
 
 <script>
-
+import Input from "../components/Input";
 
 export default {
   name: 'Home',
   components: {
-
+    Input
   },
   data() {
     return {
       tarea: {
+        id: '',
         nombre: '',
         categorias: [],
         estado: '',
@@ -104,7 +40,6 @@ export default {
       console.log('no está vacio')
       
       // envian los datos
-
       this.tarea = {
         nombre: '',
         categorias: [],
@@ -113,10 +48,5 @@ export default {
       }
     }
   },
-  computed: {
-    bloquear(){
-      return this.tarea.nombre.trim() === "" ? true : false
-    }
-  }
 }
 </script>
