d7ef82796c4567bcbb63d04c45ecf138253799cf
gubiarpa <billy.arredondo@pucp.pe>
2020-11-21 06:13:30 -0500

    Adding Tarea element

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,6 +13,8 @@
 
 <script>
 import Input from "../components/Input";
+import { mapActions } from "vuex";
+const shortid = require("shortid");
 
 export default {
   name: 'Home',
@@ -31,15 +33,21 @@ export default {
     }
   },
   methods: {
+    ...mapActions(['setTareas']),
     procesarFormulario(){
       console.log(this.tarea)
       if(this.tarea.nombre.trim() === ""){
-        console.log('Campo vacío')
+        console.log('Campo vacío');
         return
       }
-      console.log('no está vacio')
+
+      // Generar ID
+      this.tarea.id = shortid.generate();
+      console.log(this.tarea.id);
       
       // envian los datos
+      this.setTareas(this.tarea);
+
       this.tarea = {
         nombre: '',
         categorias: [],
