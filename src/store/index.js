54f6bcd1b25d0674f4a03459d77eb9dc2ccf6904
gubiarpa <billy.arredondo@pucp.pe>
2020-11-25 00:53:06 -0500

    Using Local Storage

--- src/store/index.js
+++ src/store/index.js
@@ -13,26 +13,41 @@ export default createStore({
     }
   },
   mutations: {
+    cargar(state, payload) {
+      state.tareas = payload;
+    },
     set(state, payload) {
       state.tareas.push(payload);
+      localStorage.setItem('tareas', JSON.stringify(state.tareas));
     },
     eliminar(state, payload) {
       state.tareas = state.tareas.filter(item => item.id !== payload)
+      localStorage.setItem('tareas', JSON.stringify(state.tareas));
     },
     tarea(state, payload) {
       if (!state.tareas.find(item => item.id === payload)) {
         router.push('/');
         return;
       }
-
+      
       state.tarea = state.tareas.find(item => item.id === payload);
     },
     update(state, payload) {
       state.tareas = state.tareas.map(item => item.id === payload.id ? payload : item);
+      localStorage.setItem('tareas', JSON.stringify(state.tareas));
       router.push('/');
     },
   },
   actions: {
+    cargarLocalStorage({ commit }) {
+      if (localStorage.getItem('tareas')) {
+        const tareas = JSON.parse(localStorage.getItem('tareas'));
+        commit('cargar', tareas);
+        return;
+      }
+
+      localStorage.setItem('tareas', JSON.stringify([]));
+    },
     setTareas({ commit }, tarea) {
       commit('set', tarea);
     },
