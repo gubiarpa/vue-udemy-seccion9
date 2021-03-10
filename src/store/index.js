25ade0d54ce9872c4f0201a885b3ad6355b3372b
gubiarpa <billy.arredondo@pucp.pe>
2021-03-09 23:29:33 -0500

    Revert "Edit Tarea on Firebase"
    This reverts commit 7f5d084e3ab7b60045396c24588e5f964c61f5e7.

--- src/store/index.js
+++ src/store/index.js
@@ -57,6 +57,9 @@ export default createStore({
           },
           body: JSON.stringify(tarea)
         });
+
+        const dataDB = await res.json();
+        console.log(dataDB);
       } catch (error) {
         console.error(error);
       }
@@ -68,18 +71,7 @@ export default createStore({
     setTarea({ commit }, id) {
       commit('tarea', id);
     },
-    async updateTarea({ commit }, tarea) {
-      try {
-        const res = await fetch(`https://celudar-v0-default-rtdb.firebaseio.com/tareas/${ tarea.id }.json`, {
-          method: 'PATCH',
-          headers: {
-            'Content-Type' : 'application/json'
-          },
-          body: JSON.stringify(tarea)
-        });
-      } catch (error) {
-        console.error(error);
-      }
+    updateTarea({ commit }, tarea) {
       commit('update', tarea);
     },
   },
